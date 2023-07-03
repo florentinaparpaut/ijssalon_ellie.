6d63ccc70b0da6d13a5cf00904139df0becbfbe1
florentinaparpaut <just_me_flori@yahoo.com>
2023-07-03 14:43:40 +0200

    mijn_lijst = [aardbei, vanillie, chocolade]

---
+++ IJssalon/tijdelijk.py
@@ -0,0 +1,24 @@
+mijn_lijst = ["aardbei", "vanillie", "chocolade"]
+prijzen = 3, 4, 5
+keuze = prijzen
+print (prijzen)
+
+mijn_var = "<aanbeiding>"
+a = 3
+b = 0.8
+print(a * b)
+
+mijn_var = "reclame_tekst"
+A = "Vaandag is de aanbeiding: aardbeiijse, 1 liter - slechts $ 2.4000000000000004."
+mijn_var2 = "reclame_tekst2"
+B =  "Vaandag is de aanbeiding: aardbeiijse, 1 liter - slechts $ 2.4."
+mijn_var3 = "reclame_tekst3"
+C =  "Vaandag is de aanbeiding: aardbeiijse, 1 liter - slechts $ 2.4."
+mijn_var = "reclame_tekst4"
+mijn_lijst = ["Vaandag", "is", "de", "aanbeiding:", "1 liter", " - slechts", "$", "2.4"]
+keuze = mijn_lijst[:8 ]
+el = "Vaandag is de aanbeiding: aardbeiijse, 1 liter - slechts $ 2.4."
+for i in el:
+    print(i)
+print("Vaandag is de aanbeiding: aardbeiijse, 1 liter - slechts $ 2.4.".upper() )
+print("Vaandag is de aanbeiding: aardbeiijse, 1 liter - slechts $ 2.4.".lower() )
